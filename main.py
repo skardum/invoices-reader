73b8746015e0a2117cab6b749a6b296e8db72678
mohammednabarawy@gmail.com <mohammednabarawy@gmail.com>
2023-02-02 21:54:25 +0300

    00

--- main.py
+++ main.py
@@ -1,87 +1,89 @@
 import cv2
-import numpy as np
-import base64
 import os
 import xlwt
 import pyzbar.pyzbar as pyzbar
+import base64
+import string
 
 
-def preprocess_image(image):
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def remove_non_printable(text):
 
-    # Apply thresholding to improve image quality
-    thresholded = cv2.adaptiveThreshold(
-        gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 2)
+    # Replace "☺,☻,♥,♦,♠,♣,§,¶" with ","
+    chars_to_remove = "?,☺,☻,♥,♦,♠,♣,§,¶,+,=,&"
+    for char in chars_to_remove:
+        text = text.replace(char, ",")
 
-    return thresholded
+    # Remove "," from the beginning of the string
+    if len(text) > 0 and text[0] == ",":
+        text = text[1:]
+    # Remove duplicate ","
+    text = ','.join(text.split(","))
+    cleaned_string = text.replace("\x01", ",").replace("\x02", ",").replace(
+        "\x03", ",").replace("\x04", ",").replace("\x05", ",").replace("\x0e", ",").replace("\x1e", ",").replace("\x13", ",")
+    cleaned_string = cleaned_string.replace("\x0f", ",").replace(
+        "\x14", ",").replace("\x15", ",").replace("\x06", ",").replace("\x07", ",").replace("\x19", ",").replace("\x08", ",").replace("\t", ",").replace(".00", "").replace("\x16", ",")
+    # Split the string by "," and store it into a list
+    result = cleaned_string.split(",")
+    result2 = list(filter(None, result))
+    # print(result2)
+    return result2
 
 
 def decode_qr_code(image):
     # Preprocess the image
-    preprocessed_image = preprocess_image(image)
-
-    # Create a QR code detector
-    qr_detections = pyzbar.decode(preprocessed_image)
-
-    # Loop through the QR code detections
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    threshold = cv2.adaptiveThreshold(
+        blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 2)
+    # Detect QR codes
+    qr_detections = pyzbar.decode(threshold)
     for detection in qr_detections:
         # Get the QR code data
         data = detection.data
-        text = base64.b64decode(data).decode("utf-8")
-        if text:
-            return text
+        try:
+            text = base64.b64decode(data).decode("utf-8")
+            print(text)
+            return text, threshold
+        except:
+            print("An exception occurred")
+            return None, threshold
+    return None, threshold
 
-    return None
 
+folder_path = r'C:\Users\moham\desktop\New folder (2)'
+result_folder = os.path.join(folder_path, "Results")
+if not os.path.exists(result_folder):
+    os.makedirs(result_folder)
 
-# Iterate through all the image files in a folder
-folder_path = r'New folder'
-
-# Create a new Excel file
 workbook = xlwt.Workbook()
 sheet = workbook.add_sheet("QR code data")
-
-# Write the header row
 sheet.write(0, 0, "Image File")
-sheet.write(0, 1, "QR code data")
+sheet.write(0, 1, "Name of Seller")
+sheet.write(0, 2, "VAT Number")
+sheet.write(0, 3, "Date and Time")
+sheet.write(0, 4, "Total Amount")
+sheet.write(0, 5, "VAT Amount")
 
-# Keep track of the row number
 row_num = 1
-
 for file_name in os.listdir(folder_path):
     file_path = os.path.join(folder_path, file_name)
     if os.path.isfile(file_path):
-        print("Processing:", file_path)
-
-        # Load the image
+        print(f"Processing: {file_path}")
         image = cv2.imread(file_path)
-
-        # Detect QR code in the image
-        data = decode_qr_code(image)
-
-        if data:
-            print("QR code data:", data)
-            # Write the data to the Excel sheet
+        invoice_data, threshold = decode_qr_code(image)
+        if invoice_data:
+            invoice_data = remove_non_printable(invoice_data)
+            print(invoice_data)
             sheet.write(row_num, 0, file_path)
-            sheet.write(row_num, 1, data)
-            row_num += 1
+            sheet.write(row_num, 1, invoice_data[0])
+            sheet.write(row_num, 2, invoice_data[1])
+            sheet.write(row_num, 3, invoice_data[2])
+            sheet.write(row_num, 4, invoice_data[3])
+            sheet.write(row_num, 5, invoice_data[4])
+            result_file = os.path.join(result_folder, file_name)
+            cv2.imwrite(result_file, threshold)
         else:
-            # Rotate the image and try again
-            for angle in [90, 180, 270]:
-                image_rotated = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-                data = decode_qr_code(image_rotated)
-                if data:
-                    print("QR code data (rotated {} degrees):".format(angle), data)
-                    # Write the data to the Excel sheet
-                    sheet.write(row_num, 0, file_path)
-                    sheet.write(row_num, 1, data)
-                    row_num += 1
-                    break
-            else:
-                # Write the file name to the Excel sheet (no QR code detected)
-                sheet.write(row_num, 0, file_path)
-                row_num += 1
+            print("qr not detected")
 
-# Save the Excel file
-workbook.save("qr_code_data0.xls")
+        row_num += 1
+workbook.save("QR code data.xls")
