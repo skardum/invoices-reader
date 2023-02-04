aa31998c661a10bac548a2ed04402ccdbf855c48
mohammednabarawy@gmail.com <mohammednabarawy@gmail.com>
2023-02-04 18:23:04 +0300

    00

--- gui.py
+++ gui.py
@@ -8,7 +8,53 @@ import xlwt
 import pyzbar.pyzbar as pyzbar
 import base64
 import string
-from main import remove_non_printable, decode_qr_code
+
+application_path = os.path.dirname(sys.executable)
+
+
+def remove_non_printable(text):
+
+    # Replace "☺,☻,♥,♦,♠,♣,§,¶" with ","
+    chars_to_remove = "?,☺,☻,♥,♦,♠,♣,§,¶,+,="
+    for char in chars_to_remove:
+        text = text.replace(char, ",")
+
+    # Remove "," from the beginning of the string
+    if len(text) > 0 and text[0] == ",":
+        text = text[1:]
+    # Remove duplicate ","
+    text = ','.join(text.split(","))
+    cleaned_string = text.replace("\x01", ",").replace("\x02", ",").replace(
+        "\x03", ",").replace("\x04", ",").replace("\x05", ",").replace("\x0e", ",").replace("\x1e", ",").replace("\x13", ",").replace("\x1f", ",").replace("\x1a", ",")
+    cleaned_string = cleaned_string.replace("\x0f", ",").replace(
+        "\x14", ",").replace("\x15", ",").replace("\x06", ",").replace("\x07", ",").replace("\x19", ",").replace("\x08", ",").replace("\t", ",").replace(".00", "").replace("\x16", ",")
+    # Split the string by "," and store it into a list
+    result = cleaned_string.split(",")
+    result2 = list(filter(None, result))
+    # print(result2)
+    return result2
+
+
+def decode_qr_code(image):
+    # Preprocess the image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    threshold = cv2.adaptiveThreshold(
+        blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 2)
+    # Detect QR codes
+    qr_detections = pyzbar.decode(threshold)
+    for detection in qr_detections:
+        # Get the QR code data
+        data = detection.data
+        try:
+            text = base64.b64decode(data).decode("utf-8")
+            print(text)
+            return text, threshold
+        except:
+            print("An exception occurred")
+            return "qr not detected,0,0,0,0", threshold
+
+    return "qr not detected,0,0,0,0", threshold
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -72,10 +118,10 @@ class MainWindow(QtWidgets.QMainWindow):
             sheet.write(0, 3, "Date and Time")
             sheet.write(0, 4, "Total Amount")
             sheet.write(0, 5, "VAT Amount")
-
+            num_files = len(os.listdir(folder_path))
             row_num = 1
             progress_dialog = QtWidgets.QProgressDialog(
-                "Reading invoices...", "Cancel", 0, 100, self)
+                "Reading invoices...", "Cancel", 0, num_files, self)
             progress_dialog.setWindowTitle("Reading invoices")
             progress_dialog.setWindowModality(QtCore.Qt.WindowModal)
             progress_dialog.show()
@@ -99,6 +145,13 @@ class MainWindow(QtWidgets.QMainWindow):
                         progress_dialog.setValue(row_num)
                     else:
                         print("qr not detected")
+                        sheet.write(row_num, 0, file_path)
+                        sheet.write(row_num, 1, "qr not detected")
+                        sheet.write(row_num, 2, 0)
+                        sheet.write(row_num, 3, 0)
+                        sheet.write(row_num, 4, 0)
+                        sheet.write(row_num, 5, 0)
+                        progress_dialog.setValue(row_num)
 
                     row_num += 1
             workbook.save(self.location)
